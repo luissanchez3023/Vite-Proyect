d507d6bb233d4fbaa89f1eb73c9397354e3a4566
Luis Eduardo Sanchez Reyes <luiseduardo.sanchez@globant.com>
2024-02-28 10:13:59 -0500

    Vite Proyect

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+        <div>
+          <h1>Hola, soy Luis Sanchez</h1>
+        </div>
+    
+
+    <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+    </button>    
+    </>
+  )
+}
+
+export default App
